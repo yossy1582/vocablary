537dd4216e3575d658aea3181759e71ab46b0dc8
yossy1582 <108226701+yossy1582@users.noreply.github.com>
2022-08-21 21:08:41 +0900

    d

--- vocablary.py
+++ vocablary.py
@@ -34,18 +34,30 @@ japanese = data['Japanese']
 st.write(english)
 
 num=3
-col= st.columns(num)
-for n in range(answer_num):
-    for lst in list(range(0,num,1)):
-        if check:
-            with col[lst]:
-                st.write(japanese[n])
-                st.text_input(" ")
-        else:
-            with col[lst]:
-                st.write(english[n])
-                st.text_input(" ")
-                st.write(" ")
+col1, col2, col3 = st.columns(3)
+
+for n in range(num):
+
+    with col1:
+        st.write(english[n])
+    with col2:
+        st.text_input(" ")
+    with col3:
+        st.text_input(" ")
+
+
+# col= st.columns(num)
+# for n in range(answer_num):
+#     for lst in list(range(0,num,1)):
+#         if check:
+#             with col[lst]:
+#                 st.write(japanese[n])
+#                 st.text_input(" ")
+#         else:
+#             with col[lst]:
+#                 st.write(english[n])
+#                 st.text_input(" ")
+#                 st.write(" ")
 
 
 
