e5b5a4a9ace6ec8336536f3f58c0c9b39c901331
yossy1582 <108226701+yossy1582@users.noreply.github.com>
2022-08-21 18:28:15 +0900

    d

--- vocablary.py
+++ vocablary.py
@@ -9,7 +9,8 @@ import os, sys
 pwd = os.getcwd()
 st.title("VOCABLARIES CARD " + pwd)
 
-data = pd.read_csv("/app/vocablary/data/bocablaries.csv")
+data = pd.read_csv("data/bocablaries.csv")
+# data = pd.read_csv("/app/vocablary/data/bocablaries.csv")
 st.dataframe(data)
 
 st.sidebar.title("MENU")
