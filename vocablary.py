9b4c7cc249d6a5a6b27f51321740635730ea0588
yossy1582 <108226701+yossy1582@users.noreply.github.com>
2022-08-21 18:25:33 +0900

    d

--- vocablary.py
+++ vocablary.py
@@ -2,11 +2,12 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
+import os, sys
  
 
 
-
-st.title("VOCABLARIES CARD")
+pwd = os.getcwd()
+st.title("VOCABLARIES CARD ", pwd)
 
 data = pd.read_csv("/data/vocablaries.csv")
 st.dataframe(data)
