3135de191bf492cea2928ee7e2eb7d115846da77
yossy1582 <108226701+yossy1582@users.noreply.github.com>
2022-08-21 18:47:34 +0900

    d

--- vocablary.py
+++ vocablary.py
@@ -9,7 +9,7 @@ import os, sys
 pwd = os.getcwd()
 st.title("VOCABLARIES CARD " + pwd)
 
-data = pd.read_csv("./data/vocablaries.csv", encoding='shift_jis')
+data = pd.read_csv("./data/vocablaries.csv", encoding='shift_jis', index_col=None)
 # st.dataframe(data.head())
 
 unit = data['UNIT'].values
